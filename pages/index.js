1bb721de2b79352a121be727062a1a4e2fb89491
CertifiedDoak <122711676+Anon5bPlayr@users.noreply.github.com>
2025-02-28 21:38:45 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -14,9 +14,4 @@ export default function Home() {
       <main>
         <Header title="Welcome to my app!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-        
-        {/* Add your FormComponent here */}
-        <FormComponent /> 
-      </main>
+          Get started by editing <code>pages/index.js</c
